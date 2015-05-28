045b67182a83b16aa2bbf701c3fcc88ccd0d6d71
Michał Stankiewicz <180694@edu.p.lodz.pl>
2015-05-28 09:07:58 +0200

    dodanie projektu

---
+++ ZaawansowanaJava/src/main/java/pl/mstankiewicz/zjava/ZaawansowanaJava/App.java
@@ -0,0 +1,13 @@
+package pl.mstankiewicz.zjava.ZaawansowanaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
